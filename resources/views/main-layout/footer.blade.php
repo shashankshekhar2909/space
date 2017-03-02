9ee69de730a5a36ba51469427ac8ea99b7af52a7
Shashank <sunnyrocks1122@gmail.com>
2017-03-02 20:28:27 +0530

    footer

--- resources/views/main-layout/footer.blade.php
+++ resources/views/main-layout/footer.blade.php
@@ -3,18 +3,14 @@
 footer { background-color:#0c1a1e; min-height:350px; font-family: 'Open Sans', sans-serif; }
 .footerleft { margin-top:50px; padding:0 36px; }
 .logofooter { margin-bottom:10px; font-size:25px; color:#fff; font-weight:700;}
-
 .footerleft p { color:#fff; font-size:12px !important; font-family: 'Open Sans', sans-serif; margin-bottom:15px;}
 .footerleft p i { width:20px; color:#999;}
-
-
 .paddingtop-bottom {  margin-top:50px;}
 .footer-ul { list-style-type:none;  padding-left:0px; margin-left:2px;}
 .footer-ul li { line-height:29px; font-size:12px;}
 .footer-ul li a { color:#a0a3a4; transition: color 0.2s linear 0s, background 0.2s linear 0s; }
 .footer-ul i { margin-right:10px;}
 .footer-ul li a:hover {transition: color 0.2s linear 0s, background 0.2s linear 0s; color:#ff670f; }
-
 .social:hover {
      -webkit-transform: scale(1.1);
      -moz-transform: scale(1.1);
@@ -34,7 +30,6 @@ footer { background-color:#0c1a1e; min-height:350px; font-family: 'Open Sans', s
  .bottom_ul li a { color:#FFF;  font-size:12px;}
 </style>
 <link href="https://fortawesome.github.io/Font-Awesome/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
-<!--footer start from here-->
 <footer>
   <div class="container">
     <div class="row">
@@ -78,7 +73,7 @@ footer { background-color:#0c1a1e; min-height:350px; font-family: 'Open Sans', s
 <div class="copyright">
   <div class="container">
     <div class="col-md-6">
-      <p>© 2016 - All Rights with Webenlance</p>
+      <p>© 2017 - Designed and developed by Shashank Shekhar.</p>
     </div>
     <div class="col-md-6">
       <ul class="bottom_ul">
